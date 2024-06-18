8845aa5b2543a8fe516d605c2cccc119042a719f
David SG <santome.galvan.david@gmail.com>
2024-06-19 01:24:19 +0200

    [Feature] Input component

---
+++ src/shared/types.ts
@@ -0,0 +1,40 @@
+export type Rel =
+    | "alternate"
+    | "author"
+    | "bookmark"
+    | "external"
+    | "help"
+    | "license"
+    | "next"
+    | "nofollow"
+    | "noopener"
+    | "noreferrer"
+    | "prev"
+    | "search"
+    | "tag";
+
+export type Align = "center" | "left" | "right";
+
+export type InputType =
+    | "button"
+    | "checkbox"
+    | "color"
+    | "date"
+    | "datetime-local"
+    | "email"
+    | "file"
+    | "hidden"
+    | "image"
+    | "month"
+    | "number"
+    | "password"
+    | "radio"
+    | "range"
+    | "reset"
+    | "search"
+    | "submit"
+    | "tel"
+    | "text"
+    | "time"
+    | "url"
+    | "week";
